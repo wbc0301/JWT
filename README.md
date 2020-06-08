182dd96822eef35761025ecd21604d55649eb602
wbc <417780894@qq.com>
2020-06-08 20:51:36 +0800

    优化

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## 操作步骤：
   - 安装 node 环境
   - 安装 nodemon：npm i nodemon -g
-  - 安装依赖包：npm i
+  - 安装依赖包：cnpm i
   - 在根目录中执行：npm start
   - 用浏览器打开src中的client.html文件即可访问
   
--- src/server.js
+++ src/server.js
@@ -4,48 +4,32 @@ const route = require('koa-route');
 var bodyParser = require('koa-bodyparser');
 const jwt = require('jwt-simple');
 const cors = require('@koa/cors');
-
-const secret = 'your_secret_string'; // 加密用的SECRET字符串，可随意更改
+const SECRET = 'your_secret_string'; // 加密用的SECRET字符串，可随意更改
 app.use(bodyParser()); // 处理post请求的参数
 
-// 登录接口
 const login = ctx => {
   const req = ctx.request.body;
   const userName = req.userName;
   const expires = Date.now() + 1000 * 10; // 为了方便测试，设置超时时间为一分钟后
-
-  const payload = {
-    iss: userName,
-    exp: expires
-  };
-  const Token = jwt.encode(payload, secret);  // 加密
-  console.log(Token);
-  ctx.response.body = {
-    data: Token,
-    msg: '登陆成功'
-  };
+  const payload = { iss: userName, exp: expires };
+  const Token = jwt.encode(payload, SECRET);  // 加密
+  ctx.response.body = { data: Token, msg: '登陆成功',status: 200 };
 }
 
-// 获取用户名接口
 const getUserName = ctx => {
   const token = ctx.get('authorization').split(" ")[1]; // 获取请求头，不区分大小写
-  const payload = jwt.decode(token, secret);  // 解密
-
-  // 每次请求只判断Token是否过期，不重新去更新Token过期时间(更新不更新Token的过期时间主要看实际的应用场景)
-  if (Date.now() > payload.exp) {
-    ctx.response.body = {
-      errorMsg: 'Token已过期，请重新登录'
-    };
+  const payload = jwt.decode(token, SECRET);  // 解密
+  if (Date.now() > payload.exp) {   // 每次请求只判断Token是否过期，不重新去更新Token过期时间(更新不更新Token的过期时间主要看实际的应用场景)
+    ctx.response.body = { errorMsg: 'Token已过期，请重新登录' };
   } else {
     ctx.response.body = {
-      data: {
-        username: payload.iss,
-      },
+      data: { username: payload.iss },
       msg: '获取用户名成功',
       errorMsg: ''
     };
   }
 }
+
 app.use(cors());
 app.use(route.post('/login', login));
 app.use(route.get('/getUsername', getUserName));
